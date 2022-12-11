0ef1158c09c7a7a04518e9e44d3a885f012dde83
faye <evangelion.faye@gmail.com>
2022-12-11 10:29:13 +0900

    perf👌:  display screen resolution

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,40 @@
 import Image from "next/image";
 import Haibara from "artemis/public/images/haibara.jpg";
+import { useMediaQuery } from "../hooks";
+import { LG, MD, SM, XL, XXL } from "../utils/Const";
 
 export default function Home() {
+  const isSM = useMediaQuery(SM);
+  const isMD = useMediaQuery(MD);
+  const isLG = useMediaQuery(LG);
+  const isXL = useMediaQuery(XL);
+  const isXXL = useMediaQuery(XXL);
   return (
     <>
       <main className="flex flex-col items-center p-6 md:flex-row">
         <Image src={Haibara} className="h-12 w-12 rounded-xl" alt={"baibara"} />
-        <h1 className="pt-4 text-3xl font-bold leading-none text-primary md:pt-0 md:pl-4 md:align-middle">
+        <h1 className="pt-4 text-3xl font-bold leading-none text-primary sm:pt-0 md:pt-0 md:pl-4 md:align-middle lg:pt-0 xl:pt-0 2xl:pt-0">
           Haibara Ai
         </h1>
         {/* <div className="mx-auto flex max-w-sm items-center space-x-4 rounded-xl bg-white p-6 shadow-md">
         </div> */}
         {/* <div className="shrink-0"></div> */}
       </main>
-      {/* <footer>footer</footer> */}
+      <footer className="text-center">
+        <span className="text-red-500">{`screen size: ${
+          isSM
+            ? "small"
+            : isMD
+            ? "middle"
+            : isLG
+            ? "large"
+            : isXL
+            ? "extra large"
+            : isXXL
+            ? "2extra large"
+            : "more than 2extra large"
+        }`}</span>
+      </footer>
     </>
   );
 }
---
+++ utils/Const.ts
@@ -0,0 +1,5 @@
+export const SM = 640;
+export const MD = 768;
+export const LG = 1024;
+export const XL = 1280;
+export const XXL = 1536;
